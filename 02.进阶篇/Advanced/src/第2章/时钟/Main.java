58e051d2a6ac1b40e3bc3ec695b9031297325fb0
落叶归根 <zxr10540116@outlook.com>
2017-04-14 19:56:39 +0800

    时钟

---
+++ 02.进阶篇/Advanced/src/第2章/时钟/Main.java
@@ -0,0 +1,64 @@
+package 第2章.时钟;
+
+public class Main {
+
+	public static void main(String[] args) {
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		Clock clock = new Clock(in.nextInt(), in.nextInt(), in.nextInt());
+		clock.tick();
+		System.out.println(clock);
+		in.close();
+	}
+
+}
+
+class Clock {
+	private Display hour = new Display(24);
+	private Display minute = new Display(60);
+	private Display second = new Display(60);
+	
+	Clock(int hour, int minute, int second){
+		this.hour.setValue(hour);
+		this.minute.setValue(minute);
+		this.second.setValue(second);
+	}
+	
+	public void tick(){
+		second.increase();
+		if( second.getValue() == 0) {
+			minute.increase();
+			if(minute.getValue() == 0){
+				hour.increase();
+			}
+		}
+	}
+	
+	public String toString(){
+		String s = String.format("%02d:%02d:%02d", hour.getValue(), minute.getValue(), second.getValue());
+		return s;
+	}
+}
+
+class Display {
+	private int value = 0;
+	private int limit = 0;
+	
+	public Display(int limit) {
+		this.limit = limit;
+	}
+	
+	public void increase() {
+		value++;
+		if(value == limit) {
+			value = 0;
+		}
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
